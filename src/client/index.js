b2dd30a4684af442d5079fced5155993a49a1356
Alexander Nederegger <an@MacBook-Pro.localdomain>
2020-10-22 14:29:29 +0200

    Done with basic webpack setup as per l2 of course; next up is the sass section

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,6 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-const apiKey = "e50686f90fa228c6acd6ffbf672666f6";
-
 console.log(checkForName);
 console.log(handleSubmit);
 
--- src/server/index.js
+++ src/server/index.js
@@ -7,10 +7,6 @@ const mockAPIResponse = require('./mockAPI.js')
 const dotenv = require('dotenv');
 dotenv.config();
 
-// !!! 1) Like this or ...
-const MeaningCloudAPI = new meaningCloud({
-    application_key: process.env.API_KEY
- });
 // !!! 2) ... like this?
 const API_KEY = process.env.API_KEY;
 
@@ -23,6 +19,7 @@ app.use(express.static('dist'))
 console.log(__dirname)
 
 app.get('/', function (req, res) {
+    // !!! What is the difference among the two?
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
