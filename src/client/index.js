84c70e4b053dc1eee07ecef703cf4153cc1fe8f1
Alexander Nederegger <an@MacBook-Pro.localdomain>
2020-10-24 18:14:46 +0200

    Changed .css files to .scss files and imported them in clien/index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,16 @@
+import './styles/reset.scss';
+import './styles/base.scss';
+import './styles/footer.scss';
+import './styles/form.scss';
+import './styles/header.scss';
+
+// Import own modules
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-console.log(checkForName);
-console.log(handleSubmit);
+
+console.log("Project NLP on News");
+// console.log(checkForName);
+// console.log(handleSubmit);
 
 console.log("CHANGE!!");
