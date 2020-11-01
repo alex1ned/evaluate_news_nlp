8a60f8f24edc2694719fafb840e97911e6940217
Alexander Nederegger <an@MacBook-Pro.localdomain>
2020-11-01 21:52:22 +0100

    Working on production build -- does not work atm

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,12 @@
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: ['@babel/polyfill', './src/client/index.js'],
-    output: {
-        libraryTarget: 'var',
-        library: 'Client'
-    },
     mode: 'production',
     module: {
         rules: [
@@ -18,14 +17,24 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             }
         ]
+    },
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
-    ]
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css"})
+    ],
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+    }
 }
+
+
