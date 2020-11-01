9718af27ce77c48086d1e415f52e160b6af728ac
Alexander Nederegger <an@MacBook-Pro.localdomain>
2020-11-01 22:03:44 +0100

    Working on webpack.prod.js .... build fails

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,40 +1,43 @@
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: ['@babel/polyfill', './src/client/index.js'],
     mode: 'production',
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    output: {
+        libraryTarget: 'var',
+        library: 'Client',
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.min.js'
+    },
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
             },
             {
                 test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
             }
         ]
-    },
-    output: {
-        libraryTarget: 'var',
-        library: 'Client'
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new MiniCssExtractPlugin({ filename: "[name].css"})
-    ],
-    optimization: {
-        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
-    }
+        new MiniCssExtractPlugin({filename: '[name].css'})
+    ]
 }
 
 
