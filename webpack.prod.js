fc49b352a8afc253a4a4d31844a93e5d676c1675
Alexander Nederegger <an@MacBook-Pro.localdomain>
2020-11-02 21:39:27 +0100

    final testing of webpage

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,19 +4,19 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 module.exports = {
     entry: ['@babel/polyfill', './src/client/index.js'],
     mode: 'production',
     optimization: {
-        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+        minimize: true,
+        minimizer: [new OptimizeCSSAssetsPlugin()],
     },
     output: {
         libraryTarget: 'var',
         library: 'Client',
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.min.js'
     },
     module: {
         rules: [
@@ -36,7 +36,10 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new MiniCssExtractPlugin({filename: '[name].css'})
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
 
